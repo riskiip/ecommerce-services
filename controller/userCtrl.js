9fb01035d326ba6544530cb048070dd06cc340a8
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.id>
2024-01-03 19:45:39 +0700

    fix: Fix minor issue

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -16,25 +16,13 @@ const jwt = require("jsonwebtoken");
 // Create a User ----------------------------------------------
 
 const createUser = asyncHandler(async (req, res) => {
-  /**
-   * TODO:Get the email from req.body
-   */
   const email = req.body.email;
-  /**
-   * TODO:With the help of email find the user exists or not
-   */
   const findUser = await User.findOne({ email: email });
 
   if (!findUser) {
-    /**
-     * TODO:if user not found user create a new user
-     */
     const newUser = await User.create(req.body);
     res.json(newUser);
   } else {
-    /**
-     * TODO:if user found then thow an error: User already exists
-     */
     throw new Error("User Already Exists");
   }
 });
@@ -130,9 +118,6 @@ const logout = asyncHandler(async (req, res) => {
     });
     return res.sendStatus(204); // forbidden
   }
-  await User.findOneAndUpdate(refreshToken, {
-    refreshToken: "",
-  });
   res.sendStatus(204); // forbidden
 });
 
